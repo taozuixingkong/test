387486419348718c79cabee2180b188a02ecdd46
MacBook <macbook@liuleileideMacBook-Pro.local>
2019-06-22 16:19:59 +0800

    add downloadActivity

---
+++ app/src/main/java/com/liuleilei/macbook/mydemo/activity/DownLoadActivity.java
@@ -0,0 +1,19 @@
+package com.liuleilei.macbook.mydemo.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.liuleilei.macbook.mydemo.R;
+
+/**
+ * create by liu
+ * on2019/6/22
+ */
+public class DownLoadActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download);
+    }
+}
