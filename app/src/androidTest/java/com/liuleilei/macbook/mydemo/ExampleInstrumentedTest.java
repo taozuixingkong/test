36237929ff84e37cc34da3012b380d57b4267935
MacBook <macbook@liuleileideMacBook-Pro.local>
2019-07-09 11:51:45 +0800

    save

--- app/src/androidTest/java/com/liuleilei/macbook/mydemo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/liuleilei/macbook/mydemo/ExampleInstrumentedTest.java
@@ -3,9 +3,14 @@ package com.liuleilei.macbook.mydemo;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 import static org.junit.Assert.*;
 
@@ -29,4 +34,72 @@ public class ExampleInstrumentedTest {
         int result = 1 << 1;
         System.out.print("result:"+(1<<1));
     }
+    @Test
+    public void testFor(){
+        for (;;){
+            Log.v("index","indext:");
+        }
+    }
+    @Test
+    public void test1()  {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            arrayList.add(Integer.valueOf(i));
+        }
+
+        // 复现方法一
+        Iterator<Integer> iterator = arrayList.iterator();
+        while (iterator.hasNext()) {
+            Integer integer = iterator.next();
+            if (integer.intValue() == 5) {
+                arrayList.remove(integer);
+            }
+        }
+
+        // 复现方法二
+        iterator = arrayList.iterator();
+        for (Integer value : arrayList) {
+            Integer integer = iterator.next();
+            if (integer.intValue() == 5) {
+                arrayList.remove(integer);
+            }
+        }
+    }
+    @Test
+    public void test2() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            arrayList.add(Integer.valueOf(i));
+        }
+
+        Iterator<Integer> iterator = arrayList.iterator();
+        while (iterator.hasNext()) {
+            Integer integer = iterator.next();
+            if (integer.intValue() == 5) {
+                iterator.remove();
+            }
+        }
+        int _a;
+        int String = 0;
+        String ++;
+
+    }
+    @Test
+    public void test3() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            arrayList.add(Integer.valueOf(i));
+        }
+
+        ListIterator<Integer> iterator = arrayList.listIterator();
+        while (iterator.hasNext()) {
+            Integer integer = iterator.next();
+            if (integer.intValue() == 5) {
+                iterator.set(Integer.valueOf(6));
+                iterator.remove();
+                iterator.add(integer);
+            }
+        }
+    }
+
 }
